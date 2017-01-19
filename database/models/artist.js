255df30cffae64c112f3efbe38a98daaf72965f4
jpdhernadnez <jphernandez2@myseneca.ca>
2017-01-18 22:25:39 -0500

    Create artist model and album schema

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,22 @@
-// Todo: Create Artist Model
+import mongoose from 'mongoose';
+import AlbumSchema from './album';
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    newWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [AlbumSchema]
+});
+
+
+const Artist = mongoose.model('user', ArtistSchema);
+
+module.exports = Artist;
